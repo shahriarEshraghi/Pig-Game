e94cafe3f88a94e5eeaa933a26c389ac01ebd15a
shahriar <shahriareshraghi22@Gmail.com>
2021-07-23 19:35:58 +0430

    feat(handle current score): Handle increase points

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+let currentScore = 0;
+const rollDiceBtn = document.querySelector('.btn--roll');
+
+const randomDiceRoll = function () {
+    return Math.trunc(Math.random() * 6 + 1);
+}
+rollDiceBtn.addEventListener('click', function () {
+    let diceRoll = randomDiceRoll();
+    document.querySelector('.dice').src = `dice-${diceRoll}.png`;
+    currentScore = currentScore + diceRoll;
+    if (diceRoll == 1) {
+        console.log('yek oomd ke');
+        currentScore = 0;
+    }
+    document.querySelector('.current-score').textContent = currentScore;
+
+});
+
